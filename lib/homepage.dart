c65a90b8b0cd87e28fdbb1b4a3e7f665f262c74f
iamharshkumar <hk7206210@gmail.com>
2020-06-01 15:46:28 +0530

    build homepage

---
+++ lib/homepage.dart
@@ -0,0 +1,121 @@
+import 'dart:convert';
+
+import 'package:covid19/data.dart';
+import 'package:covid19/panels/infopanel.dart';
+import 'package:covid19/panels/mosteffectedcountries.dart';
+import 'package:covid19/panels/worldwidepanel.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+class HomePage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _HomePageState();
+  }
+}
+
+class _HomePageState extends State<HomePage> {
+  Map worldData;
+  List countryData;
+
+  fetchWorldWideData() async {
+    http.Response response = await http.get('https://corona.lmao.ninja/v2/all');
+    setState(() {
+      worldData = json.decode(response.body);
+    });
+  }
+
+  fetchCountryData() async {
+    http.Response response =
+        await http.get('https://corona.lmao.ninja/v2/countries');
+    setState(() {
+      countryData = json.decode(response.body);
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    fetchWorldWideData();
+    fetchCountryData();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("COVID-19 TRACKER"),
+      ),
+      body: SingleChildScrollView(
+          child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Container(
+            alignment: Alignment.center,
+            padding: EdgeInsets.all(10),
+            height: 100,
+            color: Colors.orange[100],
+            child: Text(
+              DataSource.quote,
+              style: TextStyle(
+                  color: Colors.orange[800],
+                  fontWeight: FontWeight.bold,
+                  fontSize: 16),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Text(
+                  'Worldwide',
+                  style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+                ),
+                Container(
+                  padding: EdgeInsets.all(10),
+                  decoration: BoxDecoration(
+                      color: primaryBlack,
+                      borderRadius: BorderRadius.circular(15)),
+                  child: Text(
+                    'Regional',
+                    style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          worldData == null
+              ? CircularProgressIndicator()
+              : WorldwidePanel(worldData: worldData),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 10.0, vertical: 20),
+            child: Text(
+              'Most affected Countries',
+              style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+            ),
+          ),
+          countryData == null
+              ? Container()
+              : MostAffectedPanel(
+                  countryData: countryData,
+                ),
+          SizedBox(
+            height: 30,
+          ),
+          InfoPanel(),
+          SizedBox(
+            height: 50,
+          ),
+          Center(child: Text("WE ARE TOGETHER IN THE FIGHT", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),)),
+          SizedBox(
+            height: 50,
+          ),
+        ],
+      )),
+    );
+  }
+}
---
+++ lib/panels/infopanel.dart
@@ -0,0 +1,86 @@
+import 'package:covid19/data.dart';
+import 'package:flutter/material.dart';
+
+class InfoPanel extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        children: <Widget>[
+          Container(
+            color: primaryBlack,
+            padding: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+            margin: EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Text(
+                  "FAQS",
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 18),
+                ),
+                Icon(
+                  Icons.arrow_forward,
+                  color: Colors.white,
+                )
+              ],
+            ),
+          ),
+          GestureDetector(
+            onTap: (){
+            },
+            child: Container(
+              color: primaryBlack,
+              padding: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+              margin: EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Text(
+                    "DONATE",
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 18),
+                  ),
+                  Icon(
+                    Icons.arrow_forward,
+                    color: Colors.white,
+                  )
+                ],
+              ),
+            ),
+          ),
+          GestureDetector(
+            onTap: () {
+
+            },
+            child: Container(
+              color: primaryBlack,
+              padding: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+              margin: EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Text(
+                    "MYTH BUSTERS",
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 18),
+                  ),
+                  Icon(
+                    Icons.arrow_forward,
+                    color: Colors.white,
+                  )
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
